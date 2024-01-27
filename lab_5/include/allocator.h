7e20c564dd15a390d5c8c2f13aaaf0b6f47bf7fc
AGKravtsov <agkravtsov@mai.education>
2024-01-27 17:25:34 +0300

    lab5 v3

--- lab_5/include/allocator.h
+++ lab_5/include/allocator.h
@@ -10,6 +10,7 @@ class PoolAllocator {
         static constexpr size_t POOL_SIZE = 30000;
         T* _pool;
         std::list<T*> _free;
+        size_t _free_size;
     public:
 
         template <class U>
@@ -76,6 +77,10 @@ T* PoolAllocator<T>::allocate(){
 
 template <typename T>
 T* PoolAllocator<T>::allocate(size_t n){
+    if (n > _free_size) {
+        throw std::logic_error("Too much to allocate");
+    }
+    
     if (_free.empty()) { throw std::bad_alloc(); }
     if (_free.size() < n) { throw std::bad_alloc(); }
     for (size_t i = 0; i < n - 1; ++i) {
@@ -83,6 +88,8 @@ T* PoolAllocator<T>::allocate(size_t n){
     }
     T* tmp = _free.back();
     _free.pop_back();
+
+    _free_size -= n;
     return tmp;
 }
 
@@ -99,9 +106,16 @@ void PoolAllocator<T>::deallocate(pointer ptr, size_type n) {
     if (ptr < &_pool[0] || ptr > &_pool[0] + POOL_SIZE) {
         throw std::logic_error("Allocator does not own this pointer");
     }
+
+    if (n > POOL_SIZE - _free_size) {
+        throw std::logic_error("Too much to deallocate");
+    }
+
     T* temp = ptr;
     for (int i = 0; i < n; i++) {
         _free.push_back(temp);
         temp += sizeof(T);
     }
+
+    _free_size += n;
 }
